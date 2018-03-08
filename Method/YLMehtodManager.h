cf282d35786d889fe039652e922039a72c85a913
谭林杰 <tanlinjie@tanlinjiedeiMac.local>
2018-03-08 17:07:26 +0800

    第一次提交

---
+++ Method/YLMehtodManager.h
@@ -0,0 +1,13 @@
+//
+//  YLMehtodManager.h
+//  YLKitStoreProject
+//
+//  Created by 谭林杰 on 2018/3/8.
+//  Copyright © 2018年 Bksx-cp. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YLMehtodManager : NSObject
+
+@end
---
+++ Method/YLMehtodManager.m
@@ -0,0 +1,13 @@
+//
+//  YLMehtodManager.m
+//  YLKitStoreProject
+//
+//  Created by 谭林杰 on 2018/3/8.
+//  Copyright © 2018年 Bksx-cp. All rights reserved.
+//
+
+#import "YLMehtodManager.h"
+
+@implementation YLMehtodManager
+
+@end
